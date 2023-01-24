5eed8f30bc53481187ada769240dcb369749cdc7
noedavico <noedavico@hotmail.com>
2023-01-24 20:46:32 +0000

    entrega

--- src/js/component/lista.jsx
+++ src/js/component/lista.jsx
@@ -4,35 +4,37 @@ const Lista = () => {
   const [tarea, settarea] = useState("");
   const [arrayTareas, setArrayTareas] = useState ([]);
   
-
-  //1 PASO creo mi usuario 
-
-  // fetch('https://assets.breatheco.de/apis/fake/todos/user/noedavico87',{
-  //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //     // 'Content-Type': 'application/x-www-form-urlencoded',
-  //     },  
-  //     //body: JSON.stringify(data)
-  //     body: JSON.stringify([]) // body data type must match "Content-Type" header
-  // })//busca la info en la url pasada como valor
-  // .then((response)=>response.json())//esta linea convierte la respuesta en un json
-  // .then((data)=>console.log(data))//esta linea guarda la info transformada en un objeto en data
-  // .catch((err)=>console.log(err))//el catch te comunica si algo salió mal
+  
+   //1 PASO creo mi usuario   
+  function crearUsuario () {
+  fetch('https://assets.breatheco.de/apis/fake/todos/user/noedavico87',{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+      },  
+      //body: JSON.stringify(data)
+      body: JSON.stringify([]) // body data type must match "Content-Type" header
+  })//busca la info en la url pasada como valor
+  .then((response)=>response.json())//esta linea convierte la respuesta en un json
+  .then((data)=> {if (data.result === "ok") {getTarea()} 
+  console.log(data)})//esta linea guarda la info transformada en un objeto en data
+  .catch((err)=>console.log(err))//el catch te comunica si algo salió mal
+}
 
 
   // 2 PASO REALIZO EL GET PARA LEER LA INFORMACION
  function getTarea () {
 		fetch('https://assets.breatheco.de/apis/fake/todos/user/noedavico87',{
-			method: 'GET', // *GET, POST, PUT, DELETE, etc.
-			headers: { 'Content-Type': 'application/json'
-				// 'Content-Type': 'application/x-www-form-urlencoded',
-			  },	
+			method: 'GET', // *GET, POST, PUT, DELETE, etc.	
 		})//busca la info en la url pasada como valor
-		.then((response)=>response.json())//esta linea convierte la respuesta en un json
+		.then((response) => {console.log(response.status);
+    if (response.status === 404) {crearUsuario()}
+    return response.json()})//esta linea convierte la respuesta en un json
 		.then((data)=>setArrayTareas(data))//esta linea guarda la info transformada en un objeto
 		.catch((err)=>console.log(err))//el catch te comunica si algo salió mal
   }
+
 
   useEffect(() => {
    getTarea()
@@ -51,13 +53,14 @@ fetch('https://assets.breatheco.de/apis/fake/todos/user/noedavico87', {
 .catch(error => console.error('Error:', error));
 }
 
-
+//funcion esta atenta si el arrayTarea cambia ejecuta el put
 useEffect(() => {
   if (arrayTareas.length > 0 ) {
     actualizarTarea(arrayTareas);
   }
 }, [arrayTareas]);
  
+//fetch borrar todo 
 function deleteTarea () {
 fetch('https://assets.breatheco.de/apis/fake/todos/user/noedavico87', {
       method: 'DELETE',
@@ -110,11 +113,12 @@ const handleDelete = (borrarTarea) => {
    id="button-addon2" 
    onClick={agregartarea}>Agregar</button>
       </div>
-        <ul className="list-group glow" > {arrayTareas.map((item, index) => 
+        <ul className="list-group glow" > {arrayTareas.length > 0 ?  arrayTareas.map((item, index) => 
           <li className="list-group-item glow d-flex " key={index}> <span style={{width:"90%"}}>{item.label} </span>
             <button className="btn btn-outline-secondary " type="button"  onClick={ () => handleDelete(item)}>x</button>
            </li>
-           )} </ul>
+           ) : null } 
+           </ul>
       <div><button className="btn btn-outline-secondary" 
    type="button" 
    id="button-addon2" 
