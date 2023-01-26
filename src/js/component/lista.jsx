2bc7dafea936376f0cae208935ce6a404f2adf8d
noedavico <noedavico@hotmail.com>
2023-01-26 17:24:00 +0000

    se aplican oportunidades de mejora

--- src/js/component/lista.jsx
+++ src/js/component/lista.jsx
@@ -29,18 +29,18 @@ const Lista = () => {
 			method: 'GET', // *GET, POST, PUT, DELETE, etc.	
 		})//busca la info en la url pasada como valor
 		.then((response) => {console.log(response.status);
-    if (response.status === 404) {crearUsuario()}
+    if (response.status === 404) {crearUsuario()} //ERROR 404 usuario no existe llama a la funcion para crearlo
     return response.json()})//esta linea convierte la respuesta en un json
 		.then((data)=>setArrayTareas(data))//esta linea guarda la info transformada en un objeto
 		.catch((err)=>console.log(err))//el catch te comunica si algo salió mal
   }
 
-
+// cuando se carga la pagina lee infomrmacion al estar vacio el array 
   useEffect(() => {
    getTarea()
   },[])
 
-  //3 HAGO UN PUT PARA ACTUALIZAR 
+  //3 HAGO UN PUT PARA ACTUALIZAR LA INFORMACION
 const actualizarTarea = () => {
 fetch('https://assets.breatheco.de/apis/fake/todos/user/noedavico87', {
   method: 'PUT', // or 'POST'
@@ -65,31 +65,19 @@ function deleteTarea () {
 fetch('https://assets.breatheco.de/apis/fake/todos/user/noedavico87', {
       method: 'DELETE',
 })
-.then(res => res.json())
-.then(res=> {
-      console.log(res);
+.then(response => response.json())
+.then(response=> {
+      console.log(response);
 });
 }
 
-  //funcion atada a un evento cuando cambia  se actualiza tarea 
-  function handletarea(e) {
-    settarea(e.target.value)
-  }
-
-  function enviarDatos(e) {
-	e.preventDefault()//detiene el comportamiento predeterminado del formulario
-	  console.log(tarea);
-	  settarea("")
-  }
 
 //funcion agregar tarea 
   const agregartarea = () => {
-    if (tarea === "") {
-      alert("Debes ingresar alguna tarea")
-  } else {
-    setArrayTareas ([...arrayTareas, { label: tarea, done: false }])}
+    setArrayTareas ([...arrayTareas, { label: tarea, done: false }])
     settarea("")
-}
+  }
+
 //funcion borrar tarea
 const handleDelete = (borrarTarea) => {
   const newArray = arrayTareas.filter((item) => item !== borrarTarea);
@@ -97,36 +85,37 @@ const handleDelete = (borrarTarea) => {
 }
 
   return (
-  <div className="boxbig container col-6 ">
-     <div className="box container col-6 pagination justify-content-center"></div>
-    <p className="text-danger  fs-1 text-center ">Lista de tareas </p>
-   <div className="input-group ">
-   <input type="text" 
+  <div className="boxbig container container col-xs-8 col-sm-6 col-md-8 ">
+    <p className="text-danger  fs-1 text-center "> Lista de tareas </p>
+   <div className="input-group col-xs-8 col-sm-6 col-md-8 ">
+
+  
+   <input type="text" name="tarea"
    className="form-control" 
    placeholder="Escribir tarea" 
-   aria-label="Recipient's username" 
-   aria-describedby="button-addon2" 
-   onChange={handletarea}
-	 value={tarea}/>
+   onChange={(e) => settarea(e.target.value)}
+	 value={tarea}
+   required/>
+ 
+
    <button className="btn btn-outline-secondary" 
    type="button" 
    id="button-addon2" 
    onClick={agregartarea}>Agregar</button>
+
       </div>
         <ul className="list-group glow" > {arrayTareas.length > 0 ?  arrayTareas.map((item, index) => 
-          <li className="list-group-item glow d-flex " key={index}> <span style={{width:"90%"}}>{item.label} </span>
+          <li className="list-group-item glow d-flex " key={index}> <span style={{width:"95%"}}>{item.label} </span>
             <button className="btn btn-outline-secondary " type="button"  onClick={ () => handleDelete(item)}>x</button>
            </li>
            ) : null } 
            </ul>
-      <div><button className="btn btn-outline-secondary" 
+      <div><button className="btn btn-outline-secondary m-2" 
    type="button" 
    id="button-addon2" 
    onClick={deleteTarea}>Borrar todo</button>
    </div>
-      
-      
-    </div>
+</div>
   );
 };
 
